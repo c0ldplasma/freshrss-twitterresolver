4274898078231b7ca5652db19618331322e30133
c0ldplasma <plasma@flamari.com>
2020-07-05 19:18:30 +0200

    Fix replace

--- xExtension-TwitterResolver/extension.php
+++ xExtension-TwitterResolver/extension.php
@@ -15,28 +15,29 @@ class TwitterResolverExtension extends Minz_Extension
      */
     public function init()
     {
-        $this->registerHook('entry_before_display', array($this, 'embedYouTubeVideo'));
+        $this->registerHook('entry_before_display', array($this, 'convertTwitterUrls'));
         $this->registerTranslates();
     }
 
     /**
-     * @param string $url
+     * @param FreshRSS_Entry $entry
      * @return string
      */
-    public function convertYoutubeFeedUrl($url)
+    public function convertTwitterUrls($entry)
     {
         $matches = [];
 
-        if (preg_match('#^https?://t\.co/([0-9a-zA-Z_-]{1,64})#', $url, $matches) === 1) {
+        while (preg_match('#https?://t\.co/[0-9a-zA-Z_-]{1,64}#', $entry->content(), $matches) === 1) {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_URL, $matches[0]);
             curl_setopt($ch, CURLOPT_HEADER, true);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $a = curl_exec($ch);
             $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+            $entry->content() = str_replace($match, $url, $entry->content());
         }
 
-        return $url;
+        return $entry;
     }
 }
