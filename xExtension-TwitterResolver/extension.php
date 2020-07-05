e599317a745db2b4d3a44dcc0d49ad7f9e11de21
c0ldplasma <plasma@flamari.com>
2020-07-05 22:07:13 +0200

    Fix infinite loop when link does not resolve

--- xExtension-TwitterResolver/extension.php
+++ xExtension-TwitterResolver/extension.php
@@ -25,7 +25,6 @@ class TwitterResolverExtension extends Minz_Extension
     public function convertTwitterUrls($entry)
     {
         $matches = [];
-
         while (preg_match('#https?://t\.co/[0-9a-zA-Z_-]{1,64}#', $entry->content(), $matches) === 1) {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $matches[0]);
@@ -34,8 +33,12 @@ class TwitterResolverExtension extends Minz_Extension
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $a = curl_exec($ch);
             $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-            $entry->_content(str_replace($matches[0], '<a href="' . $url . '">' . $url . '</a>', $entry->content()));
-            $entry->_title(str_replace($matches[0], $url, $entry->content()));
+            if (strcasecmp($url, $matches[0]) !== 0) {
+                $entry->_content(str_replace($matches[0], '<a href="' . $url . '">' . $url . '</a>', $entry->content()));
+                $entry->_title(str_replace($matches[0], $url, $entry->title()));
+            } else {
+                break;
+            }
         }
 
         return $entry;
